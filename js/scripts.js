e70fa651e951aecfff380d7021cf1ca8a9e2167c
bruno <brunocarvalhorodrigues@hotmail.com>
2024-11-07 18:52:11 -0300

    removido código redundante

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,6 @@ $(document).ready(function() {
   $('#dataNascimento').mask('00/00/0000');
 });
 
-import { Calendar } from '@fullcalendar/core'
-import timeGridPlugin from '@fullcalendar/timegrid'
 
-const calendar = new Calendar(calendarEl, {
-  plugins: [timeGridPlugin],
-  initialView: 'timeGridWeek',
-  headerToolbar: {
-    left: 'prev,next',
-    center: 'title',
-    right: 'timeGridWeek,timeGridDay' // user can switch between the two
-  }
-})
 
 
